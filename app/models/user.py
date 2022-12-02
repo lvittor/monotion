28922bbf948365b34084ec22b9d71925cae5f807
lvittor <lvittor@itba.edu.ar>
2022-12-02 19:24:25 -0300

    chore: add models

--- app/models/user.py
+++ app/models/user.py
@@ -1,30 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel, Field, EmailStr
 
-class Block(BaseModel):
-    id: str = Field(..., alias = "_id")
-    type: str = Field(..., alias = "type")
-    properties: str = Field(..., alias = "properties")
-    content: Optional[list] = Field(..., alias = "content")
-    editors: Optional[list] = Field(..., alias = "editors")
-    parent: Optional[str] = Field(None, alias = "parent")
-
-    class Config:
-        allow_population_by_field_name = True
-        schema_extra = {
-            "example": {
-                "_id": "e7e93fae",
-                "type": "to_do",
-                "properties": """{
-                        "title": "Hello World",
-                        "checked": "No"
-                    }""",
-                "content": ["00315dfb", "bf2d3c32", "3070827f"],
-                "editors": ["1", "2"],
-                "parent": "2a59gah6"
-            }
-        }
-                
 class User(BaseModel):
     id: str = Field(..., alias = "_id")
     email: EmailStr = Field(..., alias = "email")
