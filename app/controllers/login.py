05362d9d40e303707735f2a8dcbb0bd4c14980f5
festevezz <festevez@itba.edu.ar>
2022-12-03 13:44:46 -0300

    feat: wip login implementation

--- app/controllers/login.py
+++ app/controllers/login.py
@@ -1,24 +1,62 @@
 import logging
 
-from fastapi import APIRouter, status
+from datetime import timedelta
+
+from fastapi import APIRouter, status, Depends
 
 from app.exceptions.http import HTTPException
 from app.views import ErrorResponse, UsersResponse
+from app.utils import MongoDBClient, UserVerificationClient
+#token should be a view?
+from app.models import User, Token
+
 
 router = APIRouter()
 log = logging.getLogger(__name__)
 
 
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
 @router.post(
     "/login",
     tags=["users"],
-    response_model=UsersResponse,
+    response_model=Token,
     summary="User login.",
     status_code=status.HTTP_200_OK,
     responses={status.HTTP_502_BAD_GATEWAY: {"model": ErrorResponse}},
 )
-async def login(email, password):
+async def login(email, password, database=Depends(MongoDBClient.get_database)):
     log.info("POST /login")
 
+    found = database.users.find_one({"email": email})
+    
+    if found is None:
+        log.error(f"Incorrect username or password.")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content=ErrorResponse(
+                code=status.HTTP_401_UNAUTHORIZED,
+                message="Incorrect username or password.",
+            ).dict(exclude_none=True),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    user = User(**found)
+    
+    if not UserVerificationClient.verify_user(user.password, password):
+        log.error(f"Incorrect username or password.")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content=ErrorResponse(
+                code=status.HTTP_401_UNAUTHORIZED,
+                message="Incorrect username or password.",
+            ).dict(exclude_none=True),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = UserVerificationClient.create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
+
     # Login user
-    return UsersResponse(properties={"password": f"{password}", "email": f"{email}"})
+    return Token(access_token=access_token, token_type="bearer")
--- app/utils/user_verification_client.py
+++ app/utils/user_verification_client.py
@@ -1,5 +1,20 @@
-from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from typing import Union
 
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+
+from app.utils import MongoDBClient
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+from app.models import TokenData
+
+SECRET_KEY = "2e98402584bcf9bb7e5c10741522ca12f3842e865e2c91a8b5df052008c2d51f"
+ALGORITHM = "HS256"
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class UserVerificationClient:
 
@@ -19,3 +34,36 @@ class UserVerificationClient:
     def verify_user(cls, user_password: str, password: str) -> bool:
         """Verify user."""
         return cls.verify_password(password, user_password)
+
+    @classmethod
+    async def get_current_user(cls, token: str = Depends(oauth2_scheme), database=Depends(MongoDBClient.get_database)):
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            email: str = payload.get("sub")
+            if email is None:
+                raise credentials_exception
+            token_data = TokenData(email=email)
+        except JWTError:
+            raise credentials_exception
+
+        user = database.users.find_one(email=token_data.email)
+        if user is None:
+            raise credentials_exception
+        return user
+
+    @classmethod
+    def create_access_token(cls, data: dict, expires_delta: Union[timedelta, None] = None):
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        return encoded_jwt
