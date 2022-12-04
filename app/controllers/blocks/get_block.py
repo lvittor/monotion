d0c28c85e25f6143a816165b505620a09c554f4d
Ezequiel Rodriguez <learodriguez@itba.edu.ar>
2022-12-04 16:24:20 -0300

    implement get block method and lint

--- app/controllers/blocks/get_block.py
+++ app/controllers/blocks/get_block.py
@@ -1,9 +1,12 @@
 import logging
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 
 from app.exceptions.http import HTTPException
-from app.views import ErrorResponse, ReadyResponse
+from app.models import User
+from app.models.block import Block, PydanticObjectId
+from app.utils import MongoDBClient, UserVerificationClient
+from app.views import BaseResponse, ErrorResponse, ReadyResponse
 
 router = APIRouter()
 log = logging.getLogger(__name__)
@@ -12,14 +15,28 @@ log = logging.getLogger(__name__)
 @router.get(
     "/block/{id}",
     tags=["blocks"],
-    response_model=ReadyResponse,
+    response_model=BaseResponse,
     summary="Get the block content.",
     status_code=status.HTTP_200_OK,
     responses={status.HTTP_502_BAD_GATEWAY: {"model": ErrorResponse}},
 )
-async def get_block(id):
+async def get_block(
+    id,
+    user: User = Depends(UserVerificationClient.get_current_user),
+    database=Depends(MongoDBClient.get_database),
+):
     log.info(f"GET /block/{id}")
+    block_id = PydanticObjectId.validate(id)
+    block = database.blocks.find_one({"_id": block_id})
+    block = Block(**block)
+    user = User(**user)
+    if id in user.get_all_allowed_blocks():
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content=ErrorResponse(
+                code=status.HTTP_401_UNAUTHORIZED,
+                message="Unauthorized access to block.",
+            ).dict(exclude_none=True),
+        )
 
-    # Get block id
-
-    return ReadyResponse(status="ok")
+    return BaseResponse(success=True, properties={"block": block.to_json()})
