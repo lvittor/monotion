56b2230403efb19f7fb204a09dd5b872bfa90d74
Ezequiel Rodriguez <learodriguez@itba.edu.ar>
2022-12-19 18:28:18 -0300

    include unregister user api call

--- app/controllers/unregister.py
+++ app/controllers/unregister.py
@@ -1,40 +1,44 @@
 import logging
 
 from fastapi import APIRouter, Depends, status
-from pymongo.errors import DuplicateKeyError
 
-from app.exceptions.http import HTTPException
-from app.models import User
+from app.controllers.blocks.delete_block import remove_block_children
+from app.models import Block, User
 from app.utils import MongoDBClient, UserVerificationClient
-from app.views import ErrorResponse, UsersResponse
+from app.views import BaseResponse, ErrorResponse
 
 router = APIRouter()
 log = logging.getLogger(__name__)
 
+
 @router.delete(
     "/unregister",
     tags=["users"],
-    response_model=UsersResponse,
+    response_model=BaseResponse,
     summary="Unregister user.",
     status_code=status.HTTP_200_OK,
     responses={status.HTTP_502_BAD_GATEWAY: {"model": ErrorResponse}},
 )
 async def unregister(
     user: User = Depends(UserVerificationClient.get_current_user),
-    database=Depends(MongoDBClient.get_database)
+    database=Depends(MongoDBClient.get_database),
 ):
     log.info("POST /unregister")
 
-    found = database.users.find_one({"email": user['email']})
-    database.users.delete_one({"_id": found["_id"]})
-
-    # Podríamos catchear la excepción de la base de datos en caso de que no se pueda
-    # hacer la modificación
-    # if not response.acknowledged:
-    #     raise Exception:
+    found = database.users.find_one({"email": user.email})
 
     deleted_user = {
         "email": f"{found['email']}",
-        "username": f"{found['username']}"
+        "username": f"{found['username']}",
+        "deleted_pages": [],
     }
-    return UsersResponse(success=True, properties={"deleted_user": deleted_user})
+
+    for page in user.pages:
+        block = Block(**database.blocks.find_one({"_id": page}))
+        remove_block_children(block, database)
+        deleted_user["deleted_pages"].append(str(page))
+        database.blocks.delete_one({"_id": page})
+
+    database.users.delete_one({"_id": found["_id"]})
+
+    return BaseResponse(success=True, properties={"deleted_user": deleted_user})
